b30a51cd249e05977c110bf01b1600d11c48b48f
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-10 22:28:53 -0500

    feat: merge branch with explore feature

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@ function Data(){
 
       Start() {
           if (window.innerWidth <= 768) { // Si la pantalla es menor o igual a 768px
+      toggle: window.innerWidth > 845, // Inicializar toggle basado en el ancho de la ventana
+
+      Start() {
+          if (window.innerWidth <= 845) { // Si la pantalla es menor o igual a 768px
               this.toggle = false; // Sobrescribir toggle a false
           }
       },
@@ -14,6 +18,8 @@ function Data(){
           return this.toggle;
       }
   }
+ }
+}
 }
 
 var swiper = new Swiper(".mySwiper", {
